dd2554a676dd83b88ad9306041d5c5d506b362be
Marina Martin <38868355+marinacmartin@users.noreply.github.com>
2021-02-25 20:17:03 -0800

    pattern lib to edit

---
+++ patternlibrarycopy/js/tabhighlighting.js
@@ -0,0 +1,27 @@
+
+
+// test for the nav underline function
+function patternpageFuntion() {
+
+  // document.getElementById("nav-pattern").style.color = "red";
+
+  //document.getElementsByClassName('underline1').style.display = "inline-block";
+  var elems = document.getElementsByClassName('patternhighlight');
+  for (var i = 0; i < elems.length; i++){
+    elems[i].style.color = '#83cdd7';
+  }
+}
+
+function citationpageFuntion() {
+  var elems = document.getElementsByClassName('citehighlight');
+  for (var i = 0; i < elems.length; i++){
+    elems[i].style.color = '#83cdd7';
+  }
+}
+
+function processpageFuntion() {
+  var elems = document.getElementsByClassName('underline4');
+  for (var i=0;i<elems.length;i+=1){
+    elems[i].style.display = 'block';
+  }
+}
