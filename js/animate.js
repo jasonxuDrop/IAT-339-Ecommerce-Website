7a67903b2616751d56095420c11d05359adde66d
Daniel Shi <xdshi@sfu.ca>
2021-03-29 17:08:42 -0700

    Added JS to onscroll for the navigation menu
    Currently only applied to product-detail

---
+++ js/animate.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$(document).ready(function(){
+    var lastScrollTop = 0;
+
+    var scrollableElement = document.body; //document.getElementById('scrollableElement');
+    var header = document.querySelector("header");
+
+    scrollableElement.addEventListener('wheel', checkScrollDirection);
+
+    function checkScrollDirection(event) {
+        if (checkScrollDirectionIsUp(event)) {
+            console.log('UP');
+            header.classList.add("sticky");
+            header.classList.remove("hide-nav");
+        } else {
+            console.log('Down');
+            header.classList.remove("sticky");
+            header.classList.add("hide-nav");
+        }
+    }
+
+    function checkScrollDirectionIsUp(event) {
+        if (event.wheelDelta) {
+            return event.wheelDelta > 0;
+        }
+        return event.deltaY < 0;
+    }
+});
--- js/product-detail.js
+++ js/product-detail.js
@@ -1,39 +1,7 @@
 "use strict";
 
 $(document).ready(function(){
-    // console.log('Width:  ' +  getWidth() );
     var element = document.getElementById("product-detail-pg-container");
-
-    // if(getWidth() < 1068){
-    //     console.log('Apply remove');
-    //     // remove max-row from hero
-    //     element.classList.remove("grid-container");
-    //     element.classList.add("grid-container-bleed");
-    //     // remove column sizing on black box
-
-    //     // remove column sizing on hero 
-
-    //     // remove detail image preview
-    //     var detailPreview = document.getElementById()
-    // } else{
-    //     element.classList.remove("grid-container-bleed");
-    //     element.classList.add("grid-container");
-    // }
-    
-
-    // function getWidth() {
-    //     if (self.innerWidth) {
-    //       return self.innerWidth;
-    //     }
-      
-    //     if (document.documentElement && document.documentElement.clientWidth) {
-    //       return document.documentElement.clientWidth;
-    //     }
-      
-    //     if (document.body) {
-    //       return document.body.clientWidth;
-    //     }
-    //   }
 
     function myFunction(x){
         if(x.matches){
